2bb887e0edd4453938af3204b3c1bf1a1ac9319d
zNadir <115375151+znadir@users.noreply.github.com>
2024-03-28 01:00:27 -0400

    make it work with bruteforce lol

--- main.py
+++ main.py
@@ -1,29 +1,43 @@
-import json
+import copy
+import random
+
 from Case import Case
 from Sudoku import Sudoku
 
-if __name__ == '__main__':
-    sudoku = Sudoku('[["5","3",".",".","7",".",".",".","."],["6",".",".","1","9","5",".",".","."],[".","9","8",".",".",".",".","6","."],["8",".",".",".","6",".",".",".","3"],["4",".",".","8",".","3",".",".","1"],["7",".",".",".","2",".",".",".","6"],[".","6",".",".",".",".","2","8","."],[".",".",".","4","1","9",".",".","5"],[".",".",".",".","8",".",".","7","9"]]')
-    print(sudoku)
-    sudoku.print_board()
-
-    cases = sudoku.extract_cases_info()
-
+def try_solve(cases):
+    filled = 0
     while True:
         undefined_cases = len(Case.get_undefined_cases(cases))
         if undefined_cases != 0:
             print(str(undefined_cases) + " wait to go")
         else:
-            break
+            return True, filled, cases
 
         cases = Case.calculate_probabilities(cases)
         try:
             cases = Case.solve_one(cases)
+            filled += 1
         except Exception as e:
-            failed_state = Sudoku.convert_to_sudoku(cases)
-            print("State before error ")
-            failed_state.print_board()
-            raise Exception(f"Failed with error {str(e)}")
+            return False, filled, cases
 
-    sudoku_solution = Sudoku.convert_to_sudoku(cases)
-    print(sudoku_solution)
+if __name__ == '__main__':
+    sudoku = Sudoku('[[".","7","4","2",".","8","5","6","1"],[".",".","2",".",".","5","3",".","8"],["1",".",".",".","7",".","4",".","."],[".",".","1",".","6","2",".",".","."],[".","4",".","3",".","1",".","5","7"],[".",".",".","4",".",".",".",".","."],[".","9",".","5",".","7","8",".","."],["2","1","8",".","3",".","7",".","5"],[".",".","3","8","2",".","1",".","6"]]')
+    print(sudoku)
+    sudoku.print_board()
+
+    initial_cases = sudoku.extract_cases_info()
+
+    while True:
+        cases = copy.deepcopy(initial_cases)
+        random.shuffle(cases)
+        is_success, filled, cases = try_solve(cases)
+
+        sudoku_state = Sudoku.convert_to_sudoku(cases)
+        sudoku_state.print_board()
+
+        if is_success == True:
+            break
+        else:
+            print(f"Failed with {str(filled)} filled cases")
+
+    print("SUCCESS")
